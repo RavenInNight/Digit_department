6146f19aeb8589d50a0daad777ca5c41b2b1de4d
Lizo4ka <karmanchuk.liza@mail.ru>
2024-04-24 23:47:25 +0300

    Add admin.py for quality_control

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+
+# Класс администратора для модели BugReport
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'created_at', 'updated_at')
+    list_filter = ('title', 'status', 'project')
+    search_fields = ('title', 'project')
+    ordering = ('created_at',)
+    date_hierarchy = 'created_at'
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'status', 'project')
+        }),
+    )
+
+    actions = ['mark_as_fixed']
+
+    def mark_as_fixed(self, request, queryset):
+        queryset.update(status='Fixed')
+
+
+# Класс администратора для модели FeatureRequest
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'status', 'created_at', 'updated_at', 'priority')
+    list_filter = ('title', 'status', 'project')
+    search_fields = ('title', 'project')
+    ordering = ('created_at',)
+    date_hierarchy = 'created_at'
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'status', 'project', 'priority')
+        }),
+    )
+
