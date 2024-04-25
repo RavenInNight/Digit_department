530675163b225be6e80cfab1b3cd8997b50513ad
Lizo4ka <karmanchuk.liza@mail.ru>
2024-04-25 09:31:51 +0300

    Add urls for quality_control

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    html = f"<h1>Система контроля качества</h1>"
+    return HttpResponse(html)
+
+def bug_list(request):
+    html = f"<h1>Cписок отчетов об ошибках</h1>"
+    return HttpResponse(html)
+
+def feature_list(request):
+    html = f"<h1>Список запросов на улучшение</h1>"
+    return HttpResponse(html)
+
+def bug_detail(request, bug_id):
+    html = f"<h1>Детали бага {bug_id}</h1>"
+    return HttpResponse(html)
+
+def feature_detail(request, feature_id):
+    html = f"<h1>Детали улучшения {feature_id}</h1>"
+    return HttpResponse(html)
