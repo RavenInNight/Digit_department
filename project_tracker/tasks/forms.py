ea969b4929b4f0ded84b1e6a2ab21c0cc380583a
Lizo4ka <karmanchuk.liza@mail.ru>
2024-05-29 11:29:31 +0300

    Add forms for project cread and add task

--- project_tracker/tasks/forms.py
+++ project_tracker/tasks/forms.py
@@ -2,17 +2,43 @@ from django import forms
 from django.forms import ModelForm
 from .models import Project, Task
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+
+
 class FeedbackForm(forms.Form):
     name = forms.CharField(label='Ваше имя', max_length=100)
     email = forms.EmailField(label='Электронная почта')
     message = forms.CharField(widget=forms.Textarea, label='Сообщение')
+
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['name', 'description']
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('name', css_class='form-control'),
+            Field('description', css_class='form-control', rows=3),
+            Submit('submit', 'Submit', css_class='btn btn-primary')
+        )
+
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'assignee']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('name', css_class='form-control'),
+            Field('description', css_class='form-control', rows=3),
+            Field('status', css_class='form-control'),
+            Field('assignee', css_class='form-control'),
+            Submit('submit', 'Submit', css_class='btn btn-primary'),
+        )
